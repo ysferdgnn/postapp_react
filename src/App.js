95d51e9a4b6363b339bb9dbd1254d03d45c004b2
ysferdgnn <ysferdgnn@gmail.com>
2022-05-04 22:24:16 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
+import { BrowserRouter, Link, Route, Router,Routes,Switch } from 'react-router-dom';
 import './App.css';
-import Post from './components/Post/Post';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import User from './components/User/User';
+
 
 function App() {
   return (
     <div className="App">
-     
-      <Post></Post>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='/users/:userId' element={<User/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
